35ace860cef4f510a0847e780b96ae8c7bbe24a3
Fer-Bar <linofernando2703@gmail.com>
2024-01-01 19:15:17 -0400

    Added pet model/admin

--- pet/admin.py
+++ pet/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from pet.models import Pet, Race
 
-# Register your models here.
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+@admin.register(Race)
+class RaceAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+class PetInline(admin.TabularInline):
+    model = Pet
+    extra = 0
+
--- pet/models.py
+++ pet/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from utils.datetools import calculate_pet_age, first_of_month
 
-# Create your models here.
+
+class Pet(models.Model):
+    name = models.CharField(max_length=40, blank=False, null=False, verbose_name=_("name"))
+    owner = models.ForeignKey("people.Person", on_delete=models.PROTECT)
+    birthday = models.DateField(null=True, blank=True, verbose_name=_("birth_date"))
+    race = models.ForeignKey(
+        "Race",
+        related_name="pet",
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        verbose_name=_("race")
+    )
+
+    @property
+    def age(self):
+        today = first_of_month(is_datetime=True)
+        age = calculate_pet_age(self.birthday, today)
+        return _(f"{age.years} years, {age.months} months.")
+
+    def __str__(self):
+        return self.name
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=40, null=False, blank=False)
