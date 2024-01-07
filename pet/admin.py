6d96d367870252a94a826af4944764d88cfbc600
Fer-Bar <linofernando2703@gmail.com>
2024-01-07 14:50:16 -0400

    Added HasOwnerFilter to PetAdmin

--- pet/admin.py
+++ pet/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from pet.filters import HasOwnerFilter
 from pet.models import Breed, Pet
 
 
@@ -8,6 +9,7 @@ class PetAdmin(admin.ModelAdmin):
     search_fields = ["name", "owner"]
     list_display = ("name", "breed", "age", "owner", "has_owner")
     readonly_fields = ("age", )
+    list_filter = (HasOwnerFilter,)
 
 
 @admin.register(Breed)
