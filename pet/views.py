884d4b265a02d95f27b8ac4bc15b753711cb6bdc
Fer-Bar <linofernando2703@gmail.com>
2024-03-19 18:30:01 -0400

    Changed the required http method to main page view

--- pet/views.py
+++ pet/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
 
+@require_http_methods(["GET"])
 def main_page(request):
     return render(request, 'pet/main_page.html')
