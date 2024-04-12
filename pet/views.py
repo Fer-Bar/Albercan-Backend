d65f5c9a9e19e7e43a4ee205ad84da7b50796460
Fer-Bar <linofernando2703@gmail.com>
2024-04-11 22:55:42 -0400

    Updated main_page view to show all pets

--- pet/views.py
+++ pet/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
+from pet.models import Pet
+
 
 @require_http_methods(["GET"])
 def main_page(request):
-    return render(request, 'pet/main_page.html')
+    pets = Pet.objects.all()
+    return render(request, "pet/main_page.html", context={"pets": pets})
