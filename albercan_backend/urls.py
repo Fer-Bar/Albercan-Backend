0ef9b2a6899fc01f9583730f515eef8881b07c18
Fer-Bar <linofernando2703@gmail.com>
2024-05-26 00:40:23 -0400

    Added auth and people urls to urlpatterns

--- albercan_backend/urls.py
+++ albercan_backend/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('pet.urls')),
+    path('', include('pet.urls', namespace='pet')),
+    path('', include('people.urls', namespace='people')),
+    path('auth/', include('authentication.urls', namespace='authentication')),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
